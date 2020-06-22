eb2aab355e7345c5fc7d1c95d73f5ab99f179069
akash-deep12 <ad952177@gmail.com>
2020-06-22 16:16:52 +0530

    second commit

---
+++ src/Login.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+        class Login extends React.Component {
+                constructor(props) {
+                  super(props);
+                  this.state = {email: '',
+                                pass: ''};
+                  this.handleChange = this.handleChange.bind(this);
+                  this.handleSubmit = this.handleSubmit.bind(this);
+                  this.handlePass = this.handlePass.bind(this);
+                }
+              
+                handleChange(event) {    this.setState({email: event.target.value});  }
+                handlePass(event) { this.setState({pass: event.target.value })}
+                handleSubmit(event) {
+                  alert('A name was submitted: ' + this.state.value);
+                  event.preventDefault();
+                }
+              
+                render() {
+                  return (
+                    <form onSubmit={this.handleSubmit} className="mt-2 container">
+                        <div className="form-group">
+                                <label> Email: </label>
+                                <input type="text" className="form-control" value={this.state.email} onChange={this.handleChange} /> 
+                        </div>
+                        <div className="form-group">
+                        <label> Password: </label>
+                        <input type="password" className="form-control" value={this.state.pass} onChange={this.handlePass} />
+                        </div>
+                      <input type="submit" value="Submit" />
+                    </form>
+                  );
+                }
+              }
+export default Login;
